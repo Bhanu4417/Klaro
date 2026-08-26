a0554a889353ba36474f72404167cdc3bb4075f6
Bhanu4417 <nagarbhanu0529@gmail.com>
2026-08-26 20:24:09 +0530

    still

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     CLERK_DISABLE_AUTO_PROXY: "true",
     NEXT_PUBLIC_CLERK_DISABLE_AUTO_PROXY: "true",
     NEXT_PUBLIC_CLERK_JS_URL: "https://cdn.jsdelivr.net/npm/@clerk/clerk-js@6/dist/clerk.browser.js",
+    NEXT_PUBLIC_CLERK_UI_URL: "https://cdn.jsdelivr.net/npm/@clerk/ui@1/dist/ui.browser.js",
   },
 };
 
