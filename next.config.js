f4cbade1a4ec93644dfd801be5388f810c7e0958
Bhanu4417 <nagarbhanu0529@gmail.com>
2026-08-26 20:13:43 +0530

    still truna fix it

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CLERK_DISABLE_AUTO_PROXY: "true",
+    NEXT_PUBLIC_CLERK_DISABLE_AUTO_PROXY: "true",
+    NEXT_PUBLIC_CLERK_JS_URL: "https://cdn.jsdelivr.net/npm/@clerk/clerk-js@6/dist/clerk.browser.js",
+  },
 };
 
 module.exports = nextConfig;
